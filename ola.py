67676122adf6316f78fbf960939b80124d9fe31a
Mariele <mariele.florencio@escola.pr.gov.br>
2026-02-09 14:11:17 +0000

    aula1

---
+++ ola.py
@@ -0,0 +1,6 @@
+#nome = input("Digite seu nome:")
+#print("Olá,",nome,"Seja bem vindo")
+
+idade= input("Qual a sua idade?")
+nome= input("Qual a sua nome?")
+print("Entendi,sua idade é",idade,"Que legal,e seu nome é",nome)
