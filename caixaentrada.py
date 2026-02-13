3b5ef22faf11b88223a2a99680d4fe060ddba5dc
Mariele <mariele.florencio@escola.pr.gov.br>
2026-02-13 11:29:25 +0000

    aula2

---
+++ caixaentrada.py
@@ -0,0 +1,3 @@
+while True:
+    try:
+        saque = int(int(""))
--- login.py
+++ login.py
@@ -1,12 +1,18 @@
 while True:
-    nome = input("Digite seu nome de usuário: ").strip()
-    senha = input ("Digite sua senha: ").strip()
+    try:
+        nome = input("Digite seu nome de usuário: ").strip()
+        senha = input("Digite sua senha: ").strip()
 
-    if nome =="":
-        print("O nome não pode ser vaszio")
-
-        if 0 <= senha <= 10: 
+        if nome == "":
+            print("O nome não pode ser vazio")
+        elif len(senha) >= 6:
             break
         else:
-            print("A senha deve ter 6 caracteries")
+            print("A senha deve ter 6 caracteres")
+    except ValueError:
+        print("Nota registrada!", senha)
+
+print("Nota registrada!", nome)
+print("Bem vindo", + nome)
+    
 
