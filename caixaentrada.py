e57c9ad84ba7daf9dc7b70b31ce6d816d72101f4
Mariele <mariele.florencio@escola.pr.gov.br>
2026-02-13 12:37:07 +0000

    aula2

--- caixaentrada.py
+++ caixaentrada.py
@@ -1,3 +1,15 @@
 while True:
     try:
-        saque = int(int(""))
+        valor = float(input("Solicite o valor do saque: "))
+
+        if valor <= 0:
+            print("O caixa só aceita números positivos!")
+        elif valor > 1000:
+            print("O caixa não permite saque maior que R$ 1000.")
+        else:
+            break # valor válido -> sai do loop
+
+    except ValueError:
+        print("Digite apenas números!")
+
+print("Saque realizado:", valor)
