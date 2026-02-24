aac1a03b1c9ec38dc5c2cadaea65c918586d588f
Mariele <mariele.florencio@escola.pr.gov.br>
2026-02-24 13:02:30 +0000

    versaodesafio

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,17 +3,36 @@ print('*********************************')
 print('*******Jogo de Adivinhação*******')
 print('*********************************')
 
-numero_secreto = random.randrange(1,101)
-total_tentativas = 10
+print("Escolha o nível de dificuldade:")
+print("(1) Fácil = 1 a 10, 10 tentativas")
+print("(2) Médio = 1 a 40, 9 tentativas")
+print("(3) Difícil = 1 a 90, 5 tentativas")
+
+nivel = int(input("Digite o nível: "))
+
+if nivel == 1:
+    numero_maximo = 10
+    total_tentativas = 10
+elif nivel == 2:
+    numero_maximo = 40
+    total_tentativas = 7
+elif nivel == 3:
+    numero_maximo = 90
+    total_tentativas = 5
+else:
+    print("Nível inválido,escolha outro")
+
+numero_secreto = random.randrange(1, numero_maximo + 1)
 rodada = 1
+
 for rodada in range (1, total_tentativas +1):
-    print("tentativa{} de {}.".format(rodada,total_tentativas))
+    print("Tentativa {} de {}.".format(rodada, total_tentativas))
     chute_str = input("Digite seu número: ")
     chute = int(chute_str)
     print("Seu número é: ", chute_str)
     
-    if(chute <1 or chute >30):
-        print("Você deve digitar um número entrei 1 e 30")
+    if(chute < 1 or chute > numero_maximo):
+        print("Você deve digitar um número entre 1 e {}".format(numero_maximo))
         continue
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
